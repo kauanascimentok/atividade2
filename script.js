2ab2b9de620c856657434960d865162fe7601845
kauanascimentok <kaua.nascimento7@aluno.senai.br>
2024-05-13 16:00:44 -0500

    subindo arquivo

---
+++ script.js
@@ -0,0 +1,12 @@
+function calcularMedia ()
+{
+let n1 = parseInt(prompt("digite um numero"))
+let n2 = parseInt(prompt("digite um numero"))
+let n3 = parseInt(prompt("digite um numero"))
+let n4 = parseInt(prompt("digite um numero"))
+
+let media = (n1 + n2 + n3 + n4) / 4;
+alert("sua media é " + media)
+
+}
+calcularMedia();
